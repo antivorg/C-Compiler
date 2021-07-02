7b2b1263232ea656ed83f6955ec394d043508363
antivorg <antivorg@yahoo.co.uk>
2021-07-02 16:23:03 +0100

    Fleshed out the state machine and created a convenient storage for tokens without requiring linked lists

--- lexing.h
+++ lexing.h
@@ -5,17 +5,17 @@
 #define LEXING.h
 
 // types
-typedef enum {
+typedef enum state {
     WHITE_SPACE, COMMENT, MULTI_LINE_COMMENT,
     TOKEN_COMPLETE,
     TOKEN_NUMERIC,
     TOKEN_IDENTIFIER,
 } state;
 
-#ifndef TYPE_token
-#define TYPE_token
-typedef struct s_token {
-    typedef enum {
+#ifndef token_t
+#define token_t
+
+typedef enum type {
         ERROR,
         IDENTIFIER,
         KEYWORD, RETURN_KEYWORD,
@@ -25,46 +25,98 @@ typedef struct s_token {
         ASSIGNMENT,
         ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION,
         LEFT_PAR, RIGHT_PAR, LEFT_BRACE, RIGHT_BRACE,
-        COMMA, SEMICOLON, 
-    } type;
-    char* value;
-    struct s_token* next;
+        COMMA, SEMICOLON
+} type;
+
+typedef struct token {
+    type tokenType;
+    char* tokenValue;
 } token;
+
 #endif
 //
 
 // functions
-token lexer(const char* src, int size);
+token* lexer(const char* src, int size);
 static int isKeyWordBool(char* name);
 static int isSpecialCharBool(char character);
-static int isOperatorBool(char* character);
+static int isOperatorBool(char character);
+static int isAlphabeticalBool(char character);
+static int isNumericalBool(char character);
 //
 
-token lexer(const char* src, int size) {
+token* lexer(const char* src, int size) {
 
     state currentState = WHITE_SPACE;
-    token baseToken;
+    token* tokens = (token*) malloc(sizeof(token*));
+
+    char* tokenValue = (char*) malloc(sizeof(char));
+    tokenValue[0] = '\0';
+
+    char buff[2] = "\0";
     int tokenNum = 0;
 
     for (int i=0; i<size; i++) {
 
         switch (currentState) {
             case WHITE_SPACE:
-                if (src[i]=='/' && src[i+1]=='/') {
+                if (src[i]=='/' && src[i+1]=='/') {//is comment
                     currentState = COMMENT;
                     i++;
-                } else if (src[i]=='/' && src[i+1]=='*') {
+                } else if (src[i]=='/' && src[i+1]=='*') {//is comment
                     currentState = MULTI_LINE_COMMENT;
                     i++;
-                } else if (isSpecialCharBool(src[i])) {
+                } else if (isSpecialCharBool(src[i])) {//is a new spec char token
                     currentState = TOKEN_COMPLETE;
-                } else if (isOperatorBool(src[i])) {
+                } else if (isOperatorBool(src[i])) {//is a new operator token
                     currentState = TOKEN_COMPLETE;
+                } else if (isNumericalBool(src[i])) {//is a neumeric
+                    currentState = TOKEN_NUMERIC;
+                } else if (isAlphabeticalBool(src[i])) {//is an identifier
+                    currentState = TOKEN_IDENTIFIER;
                 }
                 break;
+
             case TOKEN_IDENTIFIER:
+                if (isAlphabeticalBool(src[i]) || isNumericalBool(src[i])) {
+                    tokenValue = (char*) realloc(tokenValue, sizeof(tokenValue)+sizeof(char));
+                    buff[0] = src[i];
+                    tokenValue = strcat(tokenValue, buff);
+                } else {
+                    tokens = (token*) realloc(tokens, sizeof(tokens)+sizeof(token));
+                    if (strcmp(tokenValue, "return")) {
+                        tokens[tokenNum].tokenType = RETURN_KEYWORD;
+                    } else if (isKeyWordBool(tokenValue)) {
+                        tokens[tokenNum].tokenType = KEYWORD;
+                    } else {
+                        tokens[tokenNum].tokenType = IDENTIFIER;
+                    }
+
+                    tokens[tokenNum].tokenValue = tokenValue;
+
+                    tokenValue = (char*) realloc(tokenValue, sizeof(char));
+                    tokenValue[0] = '\0';
+
+                    if (src[i]=='/' && src[i+1]=='/') {//is comment
+                        currentState = COMMENT;
+                        i++;
+                    } else if (src[i]=='/' && src[i+1]=='*') {//is comment
+                        currentState = MULTI_LINE_COMMENT;
+                        i++;
+                    } else if (isSpecialCharBool(src[i])) {//is a new spec char token
+                        currentState = TOKEN_COMPLETE;
+                    } else if (isOperatorBool(src[i])) {
+                        currentState = TOKEN_COMPLETE;
+                    }
+                }
                 break;
+
             case TOKEN_NUMERIC:
+                if (isAlphabeticalBool(src[i])) {
+                    // add value
+                } else if (src[i]=='.') {
+                    // it's a boy! (float)
+                } //otherwise check for terminations
             case COMMENT:
                 if (src[i]=='\n' && src[i]=='\r') {
                     currentState = WHITE_SPACE;
@@ -87,15 +139,12 @@ static int isKeyWordBool(char* name) {
     const char* keywords[] = {
         "int", "double", "char", "void"
     };
-
     for (int i=0; i<sizeof(keywords); i++) {
         if (!strcmp(keywords[i], name)) {
             return 1;
         }
     }
-
     return 0;
-
 }
 
 static int isSpecialCharBool(char character) {
@@ -103,15 +152,12 @@ static int isSpecialCharBool(char character) {
     const char specialChars[] = {
         ';'
     };
-
     for (int i=0; i<sizeof(specialChars); i++) {
         if (specialChars[i] == character) {
             return 1;
         }
     }
-
     return 0;
-
 }
 
 static int isOperatorBool(char character) {
@@ -119,16 +165,32 @@ static int isOperatorBool(char character) {
     const char operators[] = {
         '+', '-', '*', '/'
     };
-
     for (int i=0; i<sizeof(operators); i++) {
         if (operators[i] == character) {
             return 1;
         }
     }
-
     return 0;
-
 }
 
+static int isAlphabeticalBool(char character) {
+
+    if (character >= 'a' && character <= 'z') {
+        return 1;
+    } else if (character >= 'A' && character <= 'Z') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+static int isNumericalBool(char character) {
+
+    if (character >= '9') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 #endif
